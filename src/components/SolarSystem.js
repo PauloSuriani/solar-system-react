8dc0ffc0c0b0e659172f8a568e25cc100674671b
PauloSuriani <paulosuriani@gmail.com>
2021-10-29 17:45:27 -0300

    fix: Corrige erros Lint em SolarSystem

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -3,15 +3,16 @@ import Title from './Title';
 import planets from '../data/planets';
 import PlanetCard from './PlanetCard';
 
-
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        {planets.map((planet) => 
-          <PlanetCard key={`${planet.name}`} planetName={`${planet.name}`} planetImage={`${planet.image}`} />
-        )}
+        {planets.map((planet) => (<PlanetCard
+          key={ `${planet.name}` }
+          planetName={ `${planet.name}` }
+          planetImage={ `${planet.image}` }
+        />))}
       </div>
     );
   }
